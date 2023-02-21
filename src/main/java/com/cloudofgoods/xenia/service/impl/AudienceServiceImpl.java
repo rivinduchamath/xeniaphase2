1c3bc16be9477e881ce62737474b8203c6632e15
rivinduchamath <trivinduchamath333@gmail.com>
2023-02-21 15:26:33 +0530

    Add Audiance

--- src/main/java/com/cloudofgoods/xenia/service/impl/AudienceServiceImpl.java
+++ src/main/java/com/cloudofgoods/xenia/service/impl/AudienceServiceImpl.java
@@ -3,9 +3,7 @@ package com.cloudofgoods.xenia.service.impl;
 import com.cloudofgoods.xenia.dto.AudienceDTO;
 import com.cloudofgoods.xenia.dto.response.ServiceResponseDTO;
 import com.cloudofgoods.xenia.entity.xenia.AudienceEntity;
-import com.cloudofgoods.xenia.entity.xenia.ChannelsObjects;
 import com.cloudofgoods.xenia.entity.xenia.OrganizationEntity;
-import com.cloudofgoods.xenia.entity.xenia.RuleRequestRootEntity;
 import com.cloudofgoods.xenia.models.responces.AudienceResponseObject;
 import com.cloudofgoods.xenia.repository.AudienceRepository;
 import com.cloudofgoods.xenia.repository.OrganizationRepository;
@@ -18,7 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -38,9 +38,7 @@ public class AudienceServiceImpl implements AudienceService {
                 AudienceEntity audienceEntity;
                 audienceEntity = audienceRepository.findByAudienceUuid(audienceDTO.getAudienceUuid());
                 log.info("LOG:: AudienceServiceImpl saveAudience Update");
-                audienceEntity.setAudienceUuid(audienceDTO.getAudienceUuid());
-                audienceEntity.setAudienceName(audienceDTO.getAudienceName());
-                audienceEntity.setAudienceDescription(audienceDTO.getAudienceDescription());
+                audienceEntity = setAudienceValues(audienceDTO, audienceEntity);
                 AudienceEntity save = audienceRepository.save(audienceEntity);// Update
                 serviceResponseDTO.setData(save);
                 serviceResponseDTO.setDescription("Update Audience Success");
@@ -49,40 +47,35 @@ public class AudienceServiceImpl implements AudienceService {
                 serviceResponseDTO.setHttpStatus("OK");
             } else {
                 log.info("LOG:: AudienceServiceImpl saveAudience Save");
-                rootRuleRepository.findById(audienceDTO.getCampaignUuid())
-                        .ifPresent(ruleRequestRoot -> {
-                            OrganizationEntity byUuid = organizationRepository.findByUuid(audienceDTO.getOrganizationUuid());
-                            if (byUuid != null) {
-                                byUuid.getChannelsObjects().stream()
-                                        .filter(channelsObjects -> Objects.equals(channelsObjects.getUuid(), audienceDTO.getChannelUuid()))
-                                        .findFirst()
-                                        .ifPresent(channelsObject -> {
-                                            AudienceEntity audience = new AudienceEntity();
-                                            NoArgGenerator timeBasedGenerator = Generators.timeBasedGenerator();
-                                            UUID firstUUID = timeBasedGenerator.generate();
-                                            audience.setAudienceUuid(firstUUID.toString());
-                                            audience.setAudienceName(audienceDTO.getAudienceName());
-                                            audience.setAudienceDescription(audienceDTO.getAudienceDescription());
-                                            AudienceEntity save = audienceRepository.save(audience);
-                                            serviceResponseDTO.setData(save);
-                                            serviceResponseDTO.setDescription("Save Audience Success");
-                                            serviceResponseDTO.setMessage("Success");
-                                            serviceResponseDTO.setCode("2000");
-                                            serviceResponseDTO.setHttpStatus("OK");
-                                        });
-                                if (serviceResponseDTO.getData() == null) {
-                                    serviceResponseDTO.setDescription("Save Audience Fail Channel Uuid Not Found");
-                                    serviceResponseDTO.setMessage("Fail");
-                                    serviceResponseDTO.setCode("2000");
-                                    serviceResponseDTO.setHttpStatus("OK");
-                                }
-                            } else {
-                                serviceResponseDTO.setDescription("Save Audience Fail Organization Uuid Not Found");
-                                serviceResponseDTO.setMessage("Fail");
-                                serviceResponseDTO.setCode("2000");
-                                serviceResponseDTO.setHttpStatus("OK");
-                            }
+                rootRuleRepository.findById(audienceDTO.getCampaignUuid()).ifPresent(ruleRequestRoot -> {
+                    OrganizationEntity byUuid = organizationRepository.findByUuid(audienceDTO.getOrganizationUuid());
+                    if (byUuid != null) {
+                        byUuid.getChannelsObjects().stream().filter(channelsObjects -> Objects.equals(channelsObjects.getUuid(), audienceDTO.getChannelUuid())).findFirst().ifPresent(channelsObject -> {
+                            AudienceEntity audienceEntity = new AudienceEntity();
+                            NoArgGenerator timeBasedGenerator = Generators.timeBasedGenerator();
+                            UUID firstUUID = timeBasedGenerator.generate();
+                            audienceEntity.setAudienceUuid(firstUUID.toString());
+                            audienceEntity = setAudienceValues(audienceDTO, audienceEntity);
+                            AudienceEntity save = audienceRepository.save(audienceEntity);
+                            serviceResponseDTO.setData(save);
+                            serviceResponseDTO.setDescription("Save Audience Success");
+                            serviceResponseDTO.setMessage("Success");
+                            serviceResponseDTO.setCode("2000");
+                            serviceResponseDTO.setHttpStatus("OK");
                         });
+                        if (serviceResponseDTO.getData() == null) {
+                            serviceResponseDTO.setDescription("Save Audience Fail Channel Uuid Not Found");
+                            serviceResponseDTO.setMessage("Fail");
+                            serviceResponseDTO.setCode("2000");
+                            serviceResponseDTO.setHttpStatus("OK");
+                        }
+                    } else {
+                        serviceResponseDTO.setDescription("Save Audience Fail Organization Uuid Not Found");
+                        serviceResponseDTO.setMessage("Fail");
+                        serviceResponseDTO.setCode("2000");
+                        serviceResponseDTO.setHttpStatus("OK");
+                    }
+                });
                 if (serviceResponseDTO.getData() == null) {
                     serviceResponseDTO.setDescription("Save Audience Fail Campaign Uuid Not Found");
                     serviceResponseDTO.setMessage("Fail");
@@ -101,6 +94,20 @@ public class AudienceServiceImpl implements AudienceService {
             serviceResponseDTO.setHttpStatus("OK");
             return serviceResponseDTO;
         }
+    }
+
+    AudienceEntity setAudienceValues(AudienceDTO audienceDTO, AudienceEntity newAudience) {
+        if (audienceDTO.getAudienceName() != null) newAudience.setAudienceName(audienceDTO.getAudienceName());
+        if (audienceDTO.getAudienceDescription() != null)
+            newAudience.setAudienceDescription(audienceDTO.getAudienceDescription());
+        if (audienceDTO.getAudienceJson() != null) newAudience.setAudienceJson(audienceDTO.getAudienceJson());
+        if (audienceDTO.getAudienceRuleString() != null)
+            newAudience.setAudienceRuleString(audienceDTO.getAudienceRuleString());
+        if (audienceDTO.getChannelUuid() != null) newAudience.setChannelUuid(audienceDTO.getChannelUuid());
+        if (audienceDTO.getOrganizationUuid() != null)
+            newAudience.setOrganizationUuid(audienceDTO.getOrganizationUuid());
+        if (audienceDTO.getCampaignUuid() != null) newAudience.setCampaignUuid(audienceDTO.getCampaignUuid());
+        return newAudience;
     }
 
     @Override
